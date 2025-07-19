fd67a6a9743ccdb2d4ca9f19c99266ee0c108348
Milica Jaksic <milica.jaksic14@gmail.com>
2025-07-19 14:51:13 +0200

    Put test 1

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -34,16 +34,27 @@ suite('Functional Tests', function () {
         });
     });
     // #3
-    test('Send {surname: "Colombo"}', function (done) {
+    test('send {surname: "Colombo"}', function(done) {
       chai
         .request(server)
         .keepOpen()
         .put('/travellers')
+        .send({ surname: 'Colombo' })
+        .end(function(err, res) {
+          assert.equal(res.status, 200, 'response status should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.equal(
+            res.body.name,
+            'Cristoforo',
+            'res.body.name should be "Christoforo"'
+          );
+          assert.equal(
+            res.body.surname,
+            'Colombo',
+            'res.body.surname should be "Colombo"'
+          );
 
-        .end(function (err, res) {
-          assert.fail();
-
-          done();
+          done(); 
         });
     });
     // #4
